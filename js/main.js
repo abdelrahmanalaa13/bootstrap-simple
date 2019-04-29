d25916e3bde9109626def6152084ce744941512b
Abdelrahman Alaa <abdelrahmanalaa13@users.noreply.github.com>
2019-04-30 01:42:15 +0200

    Bootstrap web page
    simple HTML/Css/Bootstrap/js/jquery web page

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(function () {
+    'use strict';
+    $('a[href^="#"]').on('click', function (e) {
+	    e.preventDefault();
+	    var target = this.hash,
+	    $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+        });
+    });
+     //start navbar code
+    var scrollTop = 0;
+    $(window).scroll(function () {
+        
+        var navbar = $('.navbar');
+        if ($(window).scrollTop() >= 50) {
+            navbar.addClass('scrolled');
+        } else {
+            navbar.removeClass('scrolled');
+        }
+    });
+     //end navbar code
+     //start form code
+    $(".form").submit(function (e) {
+        if ($(".name").val().length < 1) {
+            $(".help-name").addClass('n');
+        } else {
+            $(".help-name").removeClass('n');
+        }
+        if ($(".email").val().length < 1) {
+            $(".help-email").addClass('n');
+        } else {
+            $(".help-email").removeClass('n');
+        }
+        if ($(".num").val().length < 1) {
+            $(".help-num").addClass('n');
+        } else {
+            $(".help-num").removeClass('n');
+        }
+        if ($(".area").val().length < 1) {
+            $(".help-area").addClass('n');
+        } else {
+            $(".help-area").removeClass('n');
+        }
+        e.preventDefault();
+    });
+    //end form code
+});
